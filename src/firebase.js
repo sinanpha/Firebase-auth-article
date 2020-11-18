b63f8da621c4dbedcd8e8e849dda33a6b907e768
sinanpha <sinanpha5@gmail.com>
2020-11-18 18:15:47 +0700

    firebase-nam

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/firestore";
 import { functions } from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9WZB5N6ekNxyN3yGaUwjuBilvXItUv38",
-  authDomain: "fir-auth-article.firebaseapp.com",
-  databaseURL: "https://fir-auth-article.firebaseio.com",
-  projectId: "fir-auth-article",
-  storageBucket: "fir-auth-article.appspot.com",
-  messagingSenderId: "774252759419",
-  appId: "1:774252759419:web:e014ddfa3553a4832a15de",
-  measurementId: "G-77Z5WJ0SET"
+  apiKey: "AIzaSyAtKxPuMkhX5_gUxWOLK1eppFiTgwOMZ3w",
+  authDomain: "vd-1-59e53.firebaseapp.com",
+  databaseURL: "https://vd-1-59e53.firebaseio.com",
+  projectId: "vd-1-59e53",
+  storageBucket: "vd-1-59e53.appspot.com",
+  messagingSenderId: "61589958525",
+  appId: "1:61589958525:web:a26559d44d08c003d2e614",
+  measurementId: "G-1MVLMTY1J6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -25,13 +25,15 @@ export const signInWithGoogle = () => {
 };
 
 export const generateUserDocument = async (user, additionalData) => {
-  if (!user) return;
+  if (!user) return ;
 
   const userRef = firestore.doc(`users/${user.uid}`);
   const snapshot = await userRef.get();
+  console.log(snapshot) ;
 
   if (!snapshot.exists) {
     const { email, displayName, photoURL } = user;
+  
     try {
       await userRef.set({
         displayName,
@@ -50,7 +52,6 @@ const getUserDocument = async uid => {
   if (!uid) return null;
   try {
     const userDocument = await firestore.doc(`users/${uid}`).get();
-
     return {
       uid,
       ...userDocument.data()
--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -13,8 +13,10 @@ class UserProvider extends Component {
   
   componentDidMount = async () => {
     auth.onAuthStateChanged(async userAuth => {
+     
       const user = await generateUserDocument(userAuth);
       this.setState({ user });
+      
     });
 
 
